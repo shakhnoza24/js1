cb10cfd5290f83e64722cab2912d5891b99c785a
shakhnoza <shakhnozaxv@gmail.com>
2023-06-21 01:40:28 +0500

    js1 add

---
+++ script.js
@@ -0,0 +1,20 @@
+var userName = prompt('введите свое имя')
+console.log(typeof userName);
+
+var age = prompt('введите свой возраст')
+console.log(age);
+
+var pr1 = +prompt('8+7=?')
+console.log('Пример 1: 8 + 7 = ' + (8 + 7) + ' (Ваш ответ ' + pr1 + ')' )
+
+var pr2 = +prompt('24-10=?')
+console.log('Пример 2: 24 - 10 = ' + (24 - 10) + ' (Ваш ответ ' + pr2 + ')' )
+
+var pr3 = +prompt('4*5=?')
+console.log('Пример 3: 4 * 5 = ' + (4 * 5) + ' (Ваш ответ ' + pr3 + ')' )
+
+var pr4 = +prompt('35/7=?')
+console.log('Пример 4: 35 / 7 = ' + (35 / 7) + ' (Ваш ответ ' + pr4 + ')' )
+
+var pr5 = +prompt('30%4=?')
+console.log('Пример 5: 30 % 4 = ' + (30 % 4) + ' (Ваш ответ ' + pr5 + ')' )
